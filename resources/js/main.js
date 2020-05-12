1e1f2616e3f2315d6ed438947547488a27ca13ce
Elián Vaughn <elvaughn@Elians-MacBook.local>
2020-05-12 22:23:46 +0100

    Initial commit: Completed main page and navigation - NOT RESPONSIVE

---
+++ resources/js/main.js
@@ -0,0 +1,21 @@
+console.log('loaded!')
+
+window.onscroll = () => {
+    const navigation = document.querySelector('nav')
+    const secondaryLogo = document.querySelector('.scrolled-logo')
+    const logoSecond = document.querySelector('.scrolled-logo img')
+    const secondaryNavigation = document.querySelector('.scrolled-nav')
+    logoSecond.style.transform = 'rotate('+window.pageYOffset+'deg)';
+   if (window.scrollY > 180) {
+       secondaryLogo.classList.add('expanded-logo')
+       secondaryNavigation.classList.add('expanded-nav')
+   } else if (window.scrollY < 180 && window.scrollY > 130) {
+       secondaryLogo.classList.remove('expanded-logo')
+       secondaryNavigation.classList.remove('expanded-nav')
+       navigation.classList.add('scrolled')
+   } else {
+       navigation.classList.remove('scrolled')
+       secondaryLogo.classList.remove('expanded-logo')
+       secondaryNavigation.classList.remove('expanded-nav')
+   }
+}
