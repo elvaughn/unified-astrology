4f45507afd6b3b3eec520d47635dcc20993d5fc7
Elián Vaughn <elvaughn@Elians-MacBook.local>
2020-05-15 12:41:17 +0100

    made the index.html fully responsive

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,22 +1,35 @@
-console.log('loaded!')
+const onLoad = function() {
 
-window.onscroll = () => {
-    const navigation = document.querySelector('nav')
-    const secondaryLogo = document.querySelector('.scrolled-logo')
-    const logoSecond = document.querySelector('.scrolled-logo img')
-    const secondaryNavigation = document.querySelector('.scrolled-nav')
-    console.log(window.pageYOffset)
-    logoSecond.style.transform = 'rotate('+window.pageYOffset/6+'deg)';
-   if (window.scrollY > 180) {
-       secondaryLogo.classList.add('expanded-logo')
-       secondaryNavigation.classList.add('expanded-nav')
-   } else if (window.scrollY < 180 && window.scrollY > 130) {
-       secondaryLogo.classList.remove('expanded-logo')
-       secondaryNavigation.classList.remove('expanded-nav')
-       navigation.classList.add('scrolled')
-   } else {
-       navigation.classList.remove('scrolled')
-       secondaryLogo.classList.remove('expanded-logo')
-       secondaryNavigation.classList.remove('expanded-nav')
-   }
-}
+    // UI ELEMENTS 
+
+    const ui = {
+        mouseAnimatedIcon: document.querySelector('.mouse-scroll-icon'),
+        navigation: document.querySelector('nav'),
+        secondaryLogoDiv: document.querySelector('.scrolled-logo'),
+        secondaryLogoImg: document.querySelector('.scrolled-logo img'),
+        secondaryNavigation: document.querySelector('.scrolled-nav')
+    }
+    if (screen.width > 1005) {
+        ui.navigation.classList.add('animated-top')
+        window.onscroll = () => {
+            ui.mouseAnimatedIcon.classList.add('hidden')
+            if (window.scrollY > 200) {
+                ui.secondaryLogoImg.style.transform = 'rotate('+window.pageYOffset/4+'deg)';
+                ui.secondaryLogoDiv.classList.add('expanded-logo')
+                ui.secondaryNavigation.classList.add('expanded-nav')
+                ui.navigation.classList.add('scrolled')
+            } else {
+                ui.navigation.classList.remove('scrolled')
+                ui.secondaryLogoDiv.classList.remove('expanded-logo')
+                ui.secondaryNavigation.classList.remove('expanded-nav')
+            }
+        }
+    } else {
+        ui.navigation.classList.add('hidden')
+        ui.secondaryLogoDiv.classList.add('expanded-logo')
+        ui.mouseAnimatedIcon.classList.add('hidden')
+        window.onscroll = () => {
+            ui.secondaryLogoImg.style.transform = 'rotate('+window.pageYOffset/7+'deg)';
+        }
+    }
+}()
