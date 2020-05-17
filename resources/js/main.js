40f896de88533d3fb9038439539325fdf39ff65f
Elián Vaughn <elvaughn@Elians-MacBook.local>
2020-05-17 13:52:55 +0100

    completed overlay modifications - added logo and spin + particles

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,4 @@
-const uiController= function() {
+const uiController = function () {
     uiElements = {
         mainNav: document.querySelector('nav'),
         mainNavLogo: document.querySelector('.logo a img'),
@@ -8,22 +8,23 @@ const uiController= function() {
         mobileMenuIcon: document.querySelector('.mobile-nav-icon'),
         mobileMenuOverlay: document.querySelector('.mobile-nav-overlay'),
         mobileMenuLogo: document.querySelector('.mobile-nav-logo img'),
-        mobileNavLinks: Array.prototype.slice.call(document.querySelectorAll('.mobile-nav-overlay ul li a'))
+        mobileNavLinks: Array.prototype.slice.call(document.querySelectorAll('.mobile-nav-overlay ul li a')),
+        mobileOverlayLogo: document.querySelector('.mobile-nav-container ul li a img')
     }
     return uiElements;
 }
 
 
-const functionController = function() {
+const functionController = function () {
     const interactions = {
-        setDate: function(dateSpan) {
+        setDate: function (dateSpan) {
             dateSpan.textContent = new Date().getFullYear()
         },
-        navigationShift: function(mainNav, mainNavLogo, secondNavLogo, secondNav, mobileMenuLogo) {
+        navigationShift: function (mainNav, mainNavLogo, secondNavLogo, secondNav, mobileMenuLogo) {
             if (screen.width > 1365) {
                 window.onscroll = () => {
                     if (window.scrollY > 160) {
-                        secondNavLogo.style.transform = 'rotate('+window.pageYOffset/8+'deg)';
+                        secondNavLogo.style.transform = 'rotate(' + window.pageYOffset / 8 + 'deg)';
                         mainNav.classList.add('main-retracted');
                         secondNavLogo.classList.add('secondLogo-extended');
                         secondNav.classList.add('secondNav-extended');
@@ -35,34 +36,56 @@ const functionController = function() {
                 }
             } else if (screen.width > 1023) {
                 window.onscroll = () => {
-                    mainNavLogo.style.transform = 'rotate('+window.pageYOffset/8+'deg)';
+                    mainNavLogo.style.transform = 'rotate(' + window.pageYOffset / 8 + 'deg)';
                 }
             } else {
                 window.onscroll = () => {
-                    mobileMenuLogo.style.transform = 'rotate('+window.pageYOffset/8+'deg)';
+                    mobileMenuLogo.style.transform = 'rotate(' + window.pageYOffset / 8 + 'deg)';
                 }
             }
         },
-        mobileNavToggle: function(navIcon, overlay) {
+        mobileNavToggle: function (navIcon, overlay) {
             navIcon.addEventListener('click', () => {
-                overlay.classList.toggle('overlay-expanded')
+                if (overlay.classList.contains('overlay-expanded')) {
+                    overlay.classList.remove('overlay-expanded')
+                } else {
+                    setTimeout(() => {
+                        overlay.classList.toggle('overlay-expanded')
+                    }, 50)
+                }
             })
         },
 
-        activateMobileNavigationLinks: function(navLinks, overlay) {
+        activateMobileNavigationLinks: function (navLinks, overlay) {
             navLinks.forEach(navlink => {
                 navlink.addEventListener('click', () => {
                     overlay.classList.remove('overlay-expanded')
                 })
             })
-        }
+        },
 
+        closeOverlayOnClick: function (overlay) {
+            document.onclick = function (e) {
+                console.log(e.target.id, )
+                if (e.target.id != overlay) {
+                    if (overlay.classList.contains('overlay-expanded')) {
+                        overlay.classList.remove('overlay-expanded')
+                    }
+                }
+            }
+        },
+
+        loadParticles: function () {
+            particlesJS.load('particles-js', '/vendors/js/particles.json', function() {
+                console.log('callback - particles.js config loaded');
+            });
+        }
     }
 
     return interactions;
 }
 
-const experienceController = function(uiEl, uiInteractions) {
+const experienceController = function (uiEl, uiInteractions) {
 
     const ui = uiEl;
     const interactions = uiInteractions;
@@ -76,6 +99,10 @@ const experienceController = function(uiEl, uiInteractions) {
     interactions.mobileNavToggle(ui.mobileMenuIcon, ui.mobileMenuOverlay)
 
     interactions.activateMobileNavigationLinks(ui.mobileNavLinks, ui.mobileMenuOverlay)
+
+    interactions.closeOverlayOnClick(ui.mobileMenuOverlay)
+
+    interactions.loadParticles()
 
 
 }(uiController(), functionController())
